5109b2919b24e1b0b5e71bf4bbb1f91a11776382
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2026-05-16 03:46:09 +0600

    fix mail service render deploy

--- Nasim-Khan-Milon/backend/mail/src/index.ts
+++ Nasim-Khan-Milon/backend/mail/src/index.ts
@@ -3,14 +3,22 @@ import dotenv from 'dotenv';
 import { startSendOtpConsumer } from './consumer.js';
 
 dotenv.config();
-startSendOtpConsumer();
 
 const app = express();
+
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Mail service running");
+});
 
-app.listen(port, () => {
-  console.log(`Mail service is running on port ${port}`);
+app.listen(port, async () => {
+
+  console.log(
+    `Mail service is running on port ${port}`
+  );
+
+  await startSendOtpConsumer();
 });
